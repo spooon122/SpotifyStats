aff711f5522896a23a8c0aa2460a420fdaae351a
spooon122 <spoonchik73@gmail.com>
2024-05-09 19:53:45 +0300

    spoon branch

--- SpotifyStats/Program.cs
+++ SpotifyStats/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
